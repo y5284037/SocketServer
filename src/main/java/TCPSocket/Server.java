102562e2b1c039b66e962ba548587463006ad6bb
Arike-work <5284037@qq.com>
2018-05-22 09:24:55 +0800

    功能实现

--- src/main/java/TCPSocket/Server.java
+++ src/main/java/TCPSocket/Server.java
@@ -1,7 +1,13 @@
 package TCPSocket;
 
+import org.apache.log4j.Logger;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
 
 /**
  * @author: Arike
@@ -11,16 +17,19 @@ import java.net.Socket;
  */
 
 public class Server {
+    private static Logger logger = Logger.getLogger(Server.class);
+    private static Properties properties = new Properties();
     public static void main(String[] args) throws Exception{
+        properties.load(Object.class.getResourceAsStream("/config.properties"));
         //服务端在20006端口监听客户端请求的TCP连接
-        ServerSocket server = new ServerSocket(20006);
+        ServerSocket server = new ServerSocket(new Integer(properties.getProperty("hostPort")));
         Socket client = null;
         boolean f = true;
         while (true) {
     
             //等待客户端的连接，如果没有获取连接
             client = server.accept();
-            System.out.println("与客户端连接成功！");
+            logger.info("客户端连接成功");
             //为每个客户端连接开启一个线程
             new Thread(new ServerThread(client)).start();
         }
--- src/main/java/TCPSocket/ServerThread.java
+++ src/main/java/TCPSocket/ServerThread.java
@@ -1,7 +1,10 @@
 package TCPSocket;
 
+import org.apache.log4j.Logger;
+
 import java.io.*;
 import java.net.Socket;
+import java.util.Properties;
 
 /**
  * @author: Arike
@@ -11,8 +14,9 @@ import java.net.Socket;
  */
 
 public class ServerThread implements Runnable {
-    
+    private Logger logger = Logger.getLogger(ServerThread.class);
     private Socket client = null;
+    private Properties properties = null;
     
     public ServerThread(Socket client) {
         this.client = client;
@@ -23,21 +27,41 @@ public class ServerThread implements Runnable {
         
         //获取Socket的输入流，用来接收从客户端发送过来的数据
         DataInputStream in = null;
+        properties = new Properties();
         try {
+            properties.load(Object.class.getResourceAsStream("/config.properties"));
             in = new DataInputStream(client.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
         boolean flag = true;
-        byte[] arr = new byte[512];
+        byte[] arr = new byte[new Integer(properties.getProperty("dataSize"))];
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(properties.getProperty("dataLocation"),true)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         while (flag) {
             //接收从客户端发送过来的数据
             try {
                 assert in != null;
                 in.read(arr);
-                System.out.println(new String(arr));
+                assert bw != null;
+                bw.write(new String(arr));
+                bw.newLine();
+                bw.flush();
+                logger.info("Data Recive Success ");
+                
             } catch (Exception e) {
+                logger.info("客户端断开连接");
                 e.printStackTrace();
+                try {
+                    client.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                break;
             }
         }
     }
